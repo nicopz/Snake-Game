aeb1f2124f8097728d7008aa0cb21ac36ba845f2
nicopz <nicopolzin9@gmail.com>
2024-12-10 23:08:50 +0100

    add SnakeGame

---
+++ src/de/hsaalen/BoardTest.java
@@ -0,0 +1,13 @@
+package de.hsaalen;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BoardTest {
+
+    @Test
+    public void testConcatenate() {
+        Board board = new Board();
+        assertNotNull( board );
+    }
+}
