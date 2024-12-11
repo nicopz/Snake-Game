9f538748042fe114a1629aaa4ffff05d7b64a4f2
nicopz <nicopolzin9@gmail.com>
2024-12-11 17:06:40 +0100

    2a)refactored RAND_POS to functions maximum_tile_index_x() and maximum_tile_index_y()

--- src/de/hsaalen/Board.java
+++ src/de/hsaalen/Board.java
@@ -17,12 +17,10 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
-    private final int width_in_pixels = 300;
-    private final int height_in_pixels = 300;
-    private final int tile_size_in_pixels = 10;
-    private final int maximum_snake_length = 900;
-    private final int maximum_tile_index_x = 29;
-    private final int maximum_tile_index_y = 29;
+    public final int width_in_pixels = 300;
+    public final int height_in_pixels = 300;
+    public final int tile_size_in_pixels = 10;
+    public final int maximum_snake_length = 900;
     private final int DELAY = 140;
 
     private final int x[] = new int[maximum_snake_length];
@@ -189,12 +187,22 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
+    public int maximum_tile_index_x()
+    {
+        return ( width_in_pixels / tile_size_in_pixels ) - 1;
+    }
+
+    public int maximum_tile_index_y()
+    {
+        return ( height_in_pixels / tile_size_in_pixels ) - 1;
+    }
+
     private void locateApple() {
 
-        int r = (int) (Math.random() * maximum_tile_index_x);
+        int r = (int) (Math.random() * maximum_tile_index_x());
         apple_x = ((r * tile_size_in_pixels));
 
-        r = (int) (Math.random() * maximum_tile_index_y);
+        r = (int) (Math.random() * maximum_tile_index_y());
         apple_y = ((r * tile_size_in_pixels));
     }
 
--- src/de/hsaalen/BoardTest.java
+++ src/de/hsaalen/BoardTest.java
@@ -6,6 +6,21 @@ import static org.junit.Assert.*;
 public class BoardTest {
 
     @Test
+    public void  test_maximum_tile_index_x()
+    {
+        Board board = new Board();
+        int maximum_tile_index_x = board.maximum_tile_index_x();
+        assertEquals( ( maximum_tile_index_x + 1 ) * board.tile_size_in_pixels, board.width_in_pixels );
+    }
+
+    @Test
+    public void  test_maximum_tile_index_y()
+    {
+        Board board = new Board();
+        int maximum_tile_index_y = board.maximum_tile_index_y();
+        assertEquals( ( maximum_tile_index_y + 1 ) * board.tile_size_in_pixels, board.width_in_pixels );
+    }
+    @Test
     public void testConcatenate() {
         Board board = new Board();
         assertNotNull( board );
