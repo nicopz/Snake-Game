599f586aa6468d75781997b82dda54606d6042d4
nicopz <nicopolzin9@gmail.com>
2024-12-12 15:11:22 +0100

    2c) created class Apple

---
+++ src/de/hsaalen/Apple.java
@@ -0,0 +1,26 @@
+package de.hsaalen;
+
+public class Apple {
+    public int x;
+    public int y;
+
+    public Apple(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
